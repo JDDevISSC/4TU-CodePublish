bcf2d3303f878ab597011982f10c002af86c4c90
Jori van Dam <j.van.dam@issc.leidenuniv.nl>
2024-11-11 17:05:25 +0100

    Fixed some logging problems.

--- src/util/metadata_helper.py
+++ src/util/metadata_helper.py
@@ -9,11 +9,11 @@ def prepare_dataset_metadata(metadata_path):
     """Prepares metadata for use with the DatasetAPI by reading JSON from a file
     
     """
-    logging.debug(f"Preparing dataset metadata with metadata path: {metadata_path}")
+    logging.getLogger(prepare_dataset_metadata.__name__).debug(f"Preparing dataset metadata with metadata path: {metadata_path}")
     metadata = _load_metadata_from_file(metadata_path)
     if metadata != None or metadata == "":
-        logging.debug("Loaded metadata:")
-        logging.debug(metadata)
+        logging.getLogger(prepare_dataset_metadata.__name__).debug("Loaded metadata:")
+        logging.getLogger(prepare_dataset_metadata.__name__).debug(metadata)
         return metadata
     else:
         raise NoMetadataError
@@ -22,11 +22,11 @@ def retrieve_dataset_id_from_response(response):
     """Retrieve dataset_id from from response
     
     """
-    logging.debug(f"Retrieving dataset_id from response:")
-    logging.debug(response)
+    logging.getLogger(retrieve_dataset_id_from_response.__name__).debug(f"Retrieving dataset_id from response:")
+    logging.getLogger(retrieve_dataset_id_from_response.__name__).debug(response)
     parsed_url = urlparse(response['location'])
     uuid = parsed_url.path.split('/')[-1]
-    logging.debug(f"Found uuid ({uuid})")
+    logging.getLogger(retrieve_dataset_id_from_response.__name__).debug(f"Found uuid ({uuid})")
     return uuid
 
 def _load_metadata_from_file(path):
